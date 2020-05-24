a4bc62f1c8e8ab57e1399b2c32f244e0b2259319
Eloisa Aparicio <eloisaconsuelo.apariciocoraza@mercadolibre.com.mx>
2020-05-24 16:14:10 -0500

    on click listener

--- app/src/main/java/com/elap/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/elap/example/android/diceroller/MainActivity.kt
@@ -2,11 +2,18 @@ package com.elap.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val acceptButton: Button = findViewById(R.id.accept_button)
+        acceptButton.text = "Aceptar"
+        acceptButton.setOnClickListener{
+            Toast.makeText(this,"Hiciste click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
